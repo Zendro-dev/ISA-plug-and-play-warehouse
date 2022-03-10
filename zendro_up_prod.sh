211d5368118562140e878c3078bb8410e8aefa42
coeit <cbeiteneuer@gmail.com>
2022-03-10 12:44:52 +0100

    wip: fix: /bin/bash not working on windows

--- zendro_up_prod.sh
+++ zendro_up_prod.sh
@@ -6,25 +6,27 @@ docker cp $(pwd)/zendro_context/migration_state copycat:/opt/zendro_context
 
 docker stop copycat
 
-echo "==== \033[1;32mCreated zendro_context docker volume ====\033[0m"
+echo "==== Created zendro_context docker volume ===="
 
 # Create images required to run the migration:
 docker-compose -f docker-compose-prod.yml \
     up --no-start --force-recreate --remove-orphans \
     --build zendro_graphql_server
 
-echo "==== \033[1;32mBuild zendro_graphql_server image ====\033[0m"
+echo "==== Build zendro_graphql_server image ===="
 
 # Run the migration, which sets up keycloak for your system and copy the migration state to zendro_context volumne:
 docker-compose -f docker-compose-prod.yml \
-  run --rm zendro_graphql_server /bin/bash -c \
-  'node -e "require(\"./utils/migration\").up()" && cp zendro_migration* ../zendro_context/migration_state'
+  run --rm zendro_graphql_server node -e 'require("./utils/migration").up()'
 
-echo "==== \033[1;32mRan zendro migration and copied migration log files to zendro_context volume ====\033[0m"
+docker-compose -f docker-compose-prod.yml \
+  run --rm zendro_graphql_server cp zendro_migration* ../zendro_context/migration_state
+
+echo "==== Ran zendro migration and copied migration log files to zendro_context volume ===="
 
 # Start all Zendro services:
 docker-compose -f docker-compose-prod.yml \
     up -d --remove-orphans
 
 # Inform the user:
-echo "==== \033[1;32mAccess Zendro's GUI at http://localhost:8080 ====\033[0m"
+echo "==== Access Zendro's GUI at http://localhost:8080 ===="
